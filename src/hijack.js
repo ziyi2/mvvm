7cbef7e159ce2f63329b07c6491139c5cd7be02f
ziyi2 <673191402@qq.com>
2018-07-18 08:24:37 +0800

    refactor(hijack): 合并

--- src/hijack.js
+++ src/hijack.js
@@ -1,16 +1,11 @@
-class Hijack {
+class Observer {
   /** 
    * @Author: zhuxiankang 
    * @Date:   2018-07-17 09:09:12  
-   * @Desc:   数据劫持构造函数
-   * @Parm:   {Object} mvvm实例对象的data属性 
+   * @Desc:    
+   * @Parm:    
    */  
   constructor(data) {
     this.data = data
-    this.hijack(data)
-  }
-
-  hijack() {
-
   }
 }
